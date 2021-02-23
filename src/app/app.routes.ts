287c083f4fa04d794b502902a034c877e3802bd2
Regulator1904 <ordonezemanuel11@gmail.com>
2021-02-22 22:07:17 -0800

    updating login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from '@src/app/home/home.component';
+import { LoginComponent } from '@src/app/login/login.component';
 
 export const routes: Routes = [
   {
       path: '',
-      redirectTo: '/home',
+      redirectTo: '/login',
       pathMatch: 'full',
   },
   {
       path: 'home',
       component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: User;
+  isLoggingIn = true;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.user = new User();
+    this.user.email = "my.test.account@nativescript.org"; //FOR TESTING ONLY
+    this.user.password = "mypassword"; //FOR TESTING ONLY
+  }
+
+  signUp() {
+    this.userService.register(this.user)
+        .subscribe(
+            () => {
+                alert("Your account was successfully created.");
+                this.toggleDisplay();
+            },
+            (exception) => {
+                if (exception.error && exception.error.description) {
+                    alert(exception.error.description);
+                } else {
+                    alert(exception)
+                }
+            }
+        );
+  }
+
+  toggleDisplay() {
+      this.isLoggingIn = !this.isLoggingIn;
   }
 
 }
