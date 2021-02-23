4223713c8cf48ef9bcac68ffc171dc877824d6d5
jamesbertel <jamesalexanderbertel@gmail.com>
2021-02-23 15:49:11 -0800

    home works!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,20 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from '@src/app/home/home.component';
+import { HomeComponent } from './navigation/home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
 export const routes: Routes = [
   {
-      path: '',
-      redirectTo: '/navigation',
-      pathMatch: 'full',
-  },
-  {
-      path: 'home',
-      component: HomeComponent,
+    path: '',
+    redirectTo: '/navigation',
+    pathMatch: 'full',
   },
   {
     path: 'navigation',
     component: NavigationComponent,
-},
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
 ];
