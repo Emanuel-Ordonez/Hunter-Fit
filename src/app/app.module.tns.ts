7e9872f9648858ebe8284c431b3f69698c02da28
Regulator1904 <ordonezemanuel11@gmail.com>
2021-02-22 18:21:42 -0800

    added components

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -4,6 +4,7 @@ import { NativeScriptModule } from '@nativescript/angular';
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
+import { LoginComponent } from '@src/app/login/login.component';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding and/or HTTP wrapper
@@ -13,6 +14,7 @@ import { HomeComponent } from '@src/app/home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent
   ],
   imports: [
     NativeScriptModule,
--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from '@src/app/navigation/home/home.component';
+import { NavigationComponent } from '@src/app/navigation/navigation.component';
+import { ProfileComponent } from '@src/app/navigation/profile/profile.component';
+import { SearchComponent } from '@src/app/navigation/search/search.component';
+import { WorkoutsComponent } from '@src/app/navigation/workouts/workouts.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, NavigationComponent, ProfileComponent, SearchComponent, WorkoutsComponent],
   imports: [
     CommonModule
   ]
