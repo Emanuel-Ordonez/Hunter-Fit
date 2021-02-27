7bb6a707b543b16656e679ae8bd0a01abe8033d2
jamesbertel <jamesalexanderbertel@gmail.com>
2021-02-26 18:25:33 -0800

    cardioWorkoutComponent css started

--- src/app/navigation/workouts/start-workout/cardio-workout/cardio-workout.component.ts
+++ src/app/navigation/workouts/start-workout/cardio-workout/cardio-workout.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { EventData } from '@nativescript/core/data/observable';
+import { TextView } from '@nativescript/core/ui/text-view';
 import { WorkoutsComponent } from '../../workouts.component';
 import { StartWorkoutComponent } from '../start-workout.component';
+
 
 @Component({
   selector: 'app-cardio-workout',
@@ -10,8 +13,15 @@ import { StartWorkoutComponent } from '../start-workout.component';
 })
 export class CardioWorkoutComponent extends StartWorkoutComponent implements OnInit {
 
+  tvtext = "";
+
   ngOnInit(): void {
-    this.startWorkout();
+    // this.startWorkout();
   }
+
+  onTextChange(args: EventData) {
+    const tv = args.object as TextView;
+    // console.log(tv.text);
+}
 
 }
