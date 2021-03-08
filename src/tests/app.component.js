52cd1fe79c37c755a4a15526ee2a175c13270327
Regulator1904 <ordonezemanuel11@gmail.com>
2021-03-08 15:13:38 -0800

    testing works yay

---
+++ src/tests/app.component.js
@@ -0,0 +1,8 @@
+import "reflect-metadata";
+
+// A sample Jasmine test
+describe("A suite", function() {
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
