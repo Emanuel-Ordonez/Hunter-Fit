e998d4a135b3cf4d5cc62b03571a103715d01351
Regulator1904 <ordonezemanuel11@gmail.com>
2021-03-21 23:24:14 -0700

    search workouts is set

--- src/app/services/WorkoutConstants.ts
+++ src/app/services/WorkoutConstants.ts
@@ -1,5 +1,5 @@
 export module WorkoutConstants {
     export const buttons: Array<string> = ["walking", "running", "swimming", "biking",
-        "benchpress", "squats", "curls", "deadlifts", "rows", "pushups", "crunches", "planks", "legpress", "powerclean"];
+        "benchpress", "squats", "curls", "deadlifts", "rows", "pushups", "crunches", "planks", "legress", "powerclean"];
     export const weight_lifting_workouts: Array<string> = ["N/A", "Bench", "Squat", "Deadlift", "Lunges", "Burpies", "Curls"];
 }
