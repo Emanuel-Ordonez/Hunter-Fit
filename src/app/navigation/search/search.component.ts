dfe089a3fa79965aa524acc541e1f928b1b56dcd
Regulator1904 <ordonezemanuel11@gmail.com>
2021-03-22 15:39:37 -0700

    searching done but could add more in future

--- src/app/navigation/search/search.component.ts
+++ src/app/navigation/search/search.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RouterExtensions } from '@nativescript/angular';
 import { setString } from '@nativescript/core/application-settings';
 import { WorkoutConstants } from '@src/app/services/WorkoutConstants';
+import { EventData } from '@nativescript/core/data/observable';
+import { SearchBar } from '@nativescript/core/ui/search-bar';
 
 @Component({
   selector: 'app-search',
@@ -10,9 +12,11 @@ import { WorkoutConstants } from '@src/app/services/WorkoutConstants';
 })
 export class SearchComponent implements OnInit {
   workoutList: Array<string>;
+  searchList: Array<string>;
 
   constructor(private routerExtensions: RouterExtensions) { 
     this.workoutList = WorkoutConstants.buttons;
+    this.searchList = WorkoutConstants.buttons;
   }
 
   ngOnInit(): void {
@@ -24,5 +28,19 @@ export class SearchComponent implements OnInit {
   public startWorkout(workout: string){
     setString("workoutTypeId", workout);
     this.routerExtensions.navigate(['/weightLiftingWorkout']);
+  }
+
+  public onTextChange(args: EventData){
+    const searchBar = args.object as SearchBar;
+    this.searchWorkout(searchBar.text);
+  }
+
+  private searchWorkout(phase: string){
+    phase = phase.toLowerCase();
+    this.searchList = this.workoutList.filter(item => item.toLowerCase().match(phase));
+  }
+
+  public onClear(){
+    this.searchList=this.workoutList;
   }
 }
