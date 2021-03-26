7dc3026a105e2c0edfcbf58fd16c7904d1aafc84
Regulator1904 <ordonezemanuel11@gmail.com>
2021-03-26 14:40:53 -0700

    testing

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
         // list of files / patterns to load in the browser
         files: [
             'src/tests/**/*.js',
-            'src/app/**/*.spec.ts'
+            //'src/app/**/*.spec.ts'
         ],
 
 
